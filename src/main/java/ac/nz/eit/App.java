6f387e120df90b3975277dabd68e799860f1f214
sjdolley <garfunkalman2.gmail.com>
2019-10-15 15:15:07 +1300

    Initial Creation

---
+++ src/main/java/ac/nz/eit/App.java
@@ -0,0 +1,13 @@
+package ac.nz.eit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/ac/nz/eit/AppTest.java
@@ -0,0 +1,20 @@
+package ac.nz.eit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
