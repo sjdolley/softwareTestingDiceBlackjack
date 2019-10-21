92c8765a0104cf9f94e6168a43e981ad529ec43f
sjdolley <garfunkalman2@gmail.com>
2019-10-22 10:46:15 +1300

    changed DieTest function calls to better reflect naming conventions

--- src/test/java/ac/nz/eit/DieTest.java
+++ src/test/java/ac/nz/eit/DieTest.java
@@ -17,7 +17,7 @@ public class DieTest {
 
     // single test to check the math.random() function produces a result between 1 and 6
     @Test
-    public void checkDie_whileRollIsBetween1and6_shouldReturnTrue() {
+    public void checkRoll_whileRollIsBetween1and6_shouldReturnTrue() {
         die.roll();
         int result = die.getValue();
         assertTrue("A single Die roll should produce a result of 1-6", result >=1 && result <= 6);
