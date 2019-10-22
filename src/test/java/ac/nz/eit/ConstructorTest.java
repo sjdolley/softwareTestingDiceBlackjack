6de6e9406e1937c7a56439fb7146d67ca30e6ae1
jesse-gray <mail.jessegray@gmail.com>
2019-10-22 13:54:32 +1300

    Constructor tests

---
+++ src/test/java/ac/nz/eit/ConstructorTest.java
@@ -0,0 +1,33 @@
+package ac.nz.eit;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import org.junit.Test;
+
+public class ConstructorTest {
+
+    @Test
+    public void gameEngineConstructor_shouldReturnTrue() {
+        GameEngine testGameEngine = new GameEngine();
+        assertThat(testGameEngine, instanceOf(GameEngine.class));
+    }
+
+    @Test
+    public void dieConstructor_shouldReturnTrue() {
+        Die testDie = new Die();
+        assertThat(testDie, instanceOf(Die.class));
+    }
+
+    @Test
+    public void playerConstructor_shouldReturnTrue() {
+        Player testPlayer = new Player();
+        assertThat(testPlayer, instanceOf(Player.class));
+    }
+
+    @Test
+    public void playerConstructor_getPlayerScore_shouldReturn0() {
+        Player testPlayer = new Player();
+        assertEquals("On initialisation of Player class, score should be set to 0", testPlayer.getPlayerScore(), 0);
+    }
+}
